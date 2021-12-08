ebd85a29ac816180b35f5a1361ba6548a3f426d3
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-08 14:44:23 -0500

    18 debugged

--- hw-18.c
+++ hw-18.c
@@ -5,17 +5,18 @@
 
 int main()
 {
-    int pipes[4];
-    pipe(&pipes[0]);
-    pipe(&pipes[2]);
-    int status;
-
     while (1)
     {
+        int pipes[4];
+        pipe(&pipes[0]);
+        pipe(&pipes[2]);
+        int status;
+
         char in[1024] = {0};
         printf("Enter something for the child to destroy:\n");
-        read(STDIN_FILENO, in, sizeof(char) * 1024);
-        write(pipes[1], in, sizeof(in));
+        read(STDIN_FILENO, in, sizeof(char) * 1023);
+        write(pipes[1], in, strlen(in) * sizeof(char));
+        close(pipes[1]);
 
         int forkPid;
         if ((forkPid = fork()))
@@ -24,16 +25,26 @@ int main()
             wait(&status);
             read(pipes[2], processedLine, sizeof(processedLine));
             printf("Response: %s\n", processedLine);
+            close(pipes[2]);
+            close(pipes[3]);
         }
         else
         {
-            char in[1024] = {0};
-            read(pipes[0], in, sizeof(in));
+            char toEdit[1024] = {0};
+            read(pipes[0], toEdit, sizeof(toEdit));
+            close(pipes[0]);
             int i;
-            for (i = 0; i < 1024; i++){
-                in[i] += 1;
+            for (i = 0; i < 1024 && toEdit[i]; i++){
+                toEdit[i] += 1;
+                if (toEdit[i] == 'a' || toEdit[i] == 'A'){
+                    toEdit[i] = '4';
+                }else if (toEdit[i] == 'e' || toEdit[i] == 'E'){
+                    toEdit[i] = '3';
+                }else if (toEdit[i] == 'I' || toEdit[i] == 'i'){
+                    toEdit[i] = '1';
+                }
             }
-            write(pipes[3], in, sizeof(char) * strlen(in));
+            write(pipes[3], toEdit, sizeof(toEdit));
             return 0;
         }
     }
